ff7bfbc1726fe0fb1a0bae26036a2247adcd3576
Pinchas Tzenvirt <137898135+pt190800@users.noreply.github.com>
2025-08-19 08:36:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let start = 1000;       // סכום התחלתי
 let rate = 0.10;        // 10% ריבית
 let years = 10;         // כמה שנים
+let monthly = 200;      //הפקדה חודשית
 let amount = start;
 
 let output = "<h1>חישוב עלייה שנתית 10%</h1>";
@@ -8,7 +9,7 @@ output += "<table border='1' style='border-collapse: collapse; text-align:center
 output += "<tr><th>שנה</th><th>סכום בסוף השנה</th></tr>";
 
 for (let i = 1; i <= years; i++) {
-  amount = amount * (1 + rate); // ריבית דריבית
+  amount = amount * (1 + rate) + (monthly * 12); // ריבית דריבית
   output += `<tr><td>${i}</td><td>${amount.toFixed(2)} ₪</td></tr>`;
 }
 
